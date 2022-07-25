0a7b9e397933861451f18d47afc512cbb7f3a3e1
Maya Shankar Jha <mayashankarjha301@gmail.com>
2022-07-26 00:54:22 +0530

    Multidimentional array and its size

--- main.cpp
+++ main.cpp
@@ -16,6 +16,37 @@ int main() {
     int b = ++a;   // b = 1
     int c = a;
     cout<<a<<b<<c;
-
+//    Multi- dimentional Array
+    int x,y;
+    cin>>x>>y;
+    int arr[x][y];
+    for(int i=0; i<x; i++){
+        for(int j=0; j<y; j++){
+            cin>>arr[i][j];
+        }
+    }
+    for(int i=0; i<x; i++){
+        for(int j=0; j<y; j++){
+            cout<<arr[i][j];
+        }
+        cout<<endl;
+    }
+//    Other method to initialize a multi dimentional array
+    int arr2[2][3][4] =
+            {
+                    { {0,1,2,3}, {4,5,6,7}, {8,9,10,11} },
+                    { {12,13,14,15}, {16,17,18,19}, {20,21,22,23} }
+            };
+    // output each element's value
+    for (int i = 0; i < 2; ++i) {
+        for (int j = 0; j < 3; ++j) {
+            for (int k = 0; k < 2; ++k) {
+                cout << "Element at x[" << i << "][" << j
+                     << "][" << k << "] = " << arr2[i][j][k]
+                     << endl;
+            }
+        }
+    }
+    cout<<sizeof(arr2)<<endl;
     return 0;
 }
