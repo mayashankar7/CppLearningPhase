8b94c36c18217f596b53eb9758106489802749ee
Maya Shankar Jha <mayashankarjha301@gmail.com>
2022-07-26 00:22:49 +0530

    Use of limits.h header file

--- main.cpp
+++ main.cpp
@@ -1,8 +1,16 @@
 #include <iostream>
+#include <limits.h>
 using namespace std;
 
 int main() {
+
+//    size of operator in cpp
     cout<< sizeof(int )<<endl;
     cout<< sizeof(bool )<<endl;
+//    Max values of identifiers:- syntax<limits.h> header file is defined to find the range of fundamental data-types.
+    cout<<CHAR_MAX<<endl;
+    cout<<CHAR_MIN<<endl;
+    cout<<INT_MAX<<endl;
+
     return 0;
 }
