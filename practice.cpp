00a1f839586d0f1c0c148932c40008649fffd34e
Maya Shankar Jha <mayashankarjha301@gmail.com>
2022-08-11 17:54:00 +0530

    second largest element in an array

--- practice.cpp
+++ practice.cpp
@@ -1,72 +1,58 @@
-//  program to insert an element in an array
 #include<iostream>
-
+#include <bits/stdc++.h>
 using namespace std;
-//int insert_element(int array[], int n, int index, int capasity, int x){
-//    if(n == capasity){
-//        return n;
-//    }
-//    for(int i=n-1; i>=index; i--){
-//        array[i+1]= array[i];
-//    }
-//    array[index]=x;
-//    return (n+1);
-//}
-
-//int deleteElement(int arr[], int n, int x)
-//{
-//
-//    int i;
-//    for (i=0; i<n; i++)
-//        if (arr[i] == x)
-//            break;
-//
-//    if (i < n)
-//    {
-//        n = n - 1;
-//        for (int j=i; j<n; j++)
-//            arr[j] = arr[j+1];
-//    }
-//
-//    return n;
-//}
-//int max_element(int array[], int n){
-//    int max = 0;
-//    for(int i=0; i<n; i++){
-//        if(array[i]>array[max]) {
-//            max = array[i];
-//        }
-//    }
-//    return max;
-//
-//}
-bool  arraIsSorted(int array[] , int n){
-    bool value=false;
-    for(int i=0; i<n-1; i++){
-        if(array[i]<array[i+1]) {
-            value = true;
-        }else {
-            value = false;
-            break;
+int findLargest(int array[], int n){
+    int max=0;
+    for(int i=0; i<n; i++){
+        if(array[i]>array[max])
+            max=array[i];
+        else
+            max=array[i];
+    }
+    return max;
+}
+int secondLargest(int array[], int n){
+    int maxValue = findLargest(array,n);
+    int second = -1;
+    for(int i=0; i<n; i++){
+        if(array[i] != maxValue ){
+            if(second==-1){
+                second = i;
+            }else if(array[i]>array[second]){
+                second = i;
+            }
         }
     }
-    return value;
+    return array[second];
 }
+void secondELement(int array[], int n){
+    int largest, result;
+    largest= result= INT_MIN;
+    for(int i=0; i<n; i++){
+        if(array[i]>largest){
+            result= largest;
+            largest= array[i];
+        }else if(array[i]>result && array[i] != largest){
+            result= array[i];
+        }
+    }
+    if(result== INT_MIN){
+        cout<<"NO MINIMUM"<<endl;
+    } else{
+        cout<<result<<endl;
+    }
+}
+
 int main(){
     int n;
     cin>>n;
     int array[n];
-    for(int i=0; i<n ; i++){
+    for(int i=0; i<n; i++){
         cin>>array[i];
     }
-//    n = deleteElement(array, n, 12);
-//    for(int i=0; i<n; i++){
-//        cout<<array[i]<<" ";
-//    }
-
-//    cout<<max_element(array, n)<<endl;
-
-    cout<<arraIsSorted(array,n)<<endl;
-
-
+    cout<<findLargest(array,n)<<endl;
+    cout<<secondLargest(array,n)<<endl;
+    sort(array,array+n);
+    secondELement(array, n);
+    return 0;
 }
