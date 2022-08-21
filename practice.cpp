e78e65652ed833fcb4bb2b9c85e3c095f50f63f0
Maya Shankar Jha <mayashankarjha301@gmail.com>
2022-08-21 10:23:48 +0530

    Atm Project

--- practice.cpp
+++ practice.cpp
@@ -1,58 +1,25 @@
 #include<iostream>
-#include <bits/stdc++.h>
 using namespace std;
-int findLargest(int array[], int n){
-    int max=0;
-    for(int i=0; i<n; i++){
-        if(array[i]>array[max])
-            max=array[i];
-        else
-            max=array[i];
+class Maya2{
+private:
+    int age;
+public:
+    int salary;
+//    setters
+    void setData(int a, int s){
+        age = a;
+        salary= s;
     }
-    return max;
-}
-int secondLargest(int array[], int n){
-    int maxValue = findLargest(array,n);
-    int second = -1;
-    for(int i=0; i<n; i++){
-        if(array[i] != maxValue ){
-            if(second==-1){
-                second = i;
-            }else if(array[i]>array[second]){
-                second = i;
-            }
-        }
+    int getAge(){
+        return age;
     }
-    return array[second];
-}
-void secondELement(int array[], int n){
-    int largest, result;
-    largest= result= INT_MIN;
-    for(int i=0; i<n; i++){
-        if(array[i]>largest){
-            result= largest;
-            largest= array[i];
-        }else if(array[i]>result && array[i] != largest){
-            result= array[i];
-        }
+    int getSalary(){
+        return salary;
     }
-    if(result== INT_MIN){
-        cout<<"NO MINIMUM"<<endl;
-    } else{
-        cout<<result<<endl;
-    }
-}
-
+};
 int main(){
-    int n;
-    cin>>n;
-    int array[n];
-    for(int i=0; i<n; i++){
-        cin>>array[i];
-    }
-    cout<<findLargest(array,n)<<endl;
-    cout<<secondLargest(array,n)<<endl;
-    sort(array,array+n);
-    secondELement(array, n);
-    return 0;
+    Maya2 m1 ;
+    m1.setData(22,55000);
+    cout<<m1.getAge()<<" "<<m1.getSalary()<<endl;
+
 }
